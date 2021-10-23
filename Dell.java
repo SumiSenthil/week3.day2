784303c23360b153c6eea5af0ac0546d876c4025
SumiSenthil <91725252+SumiSenthil@users.noreply.github.com>
2021-10-23 10:38:35 +0530

    Add files via upload

---
+++ Dell.java
@@ -0,0 +1,13 @@
+package week3.day2;
+
+public class Dell extends Desktop{		
+	
+	public int softwareResources() {		
+		return softwareResources()+4;	
+	}
+
+	public int hardwareResources() {
+		return hardwareResources()+5;
+	}
+
+}
---
+++ Desktop.java
@@ -0,0 +1,25 @@
+package week3.day2;
+
+public abstract class Desktop implements Software, Hardware {
+		
+	
+	public int softwareResources()
+		{
+		return 44;	
+		}
+			
+		
+		public int hardwareResources()
+		{
+		return 35;
+		}
+		
+		public void CPU()
+		{
+		}
+		
+		
+		
+	}
+	
+
